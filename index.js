24f13a9a4505e8c09ae5b8c3c777cc93f7866fff
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-04 17:25:54 +0600

    required files installed and imported

---
+++ index.js
@@ -0,0 +1,10 @@
+// required modules
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+
+
