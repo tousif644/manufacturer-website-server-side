faf5983a0fc4fca1fd2cd0d93479d42a50db2370
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-04 17:33:40 +0600

    Simple api created , port defined, made a nodemon run script

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+// port
+const port = process.env.PORT || 5000;
 
+// apis
+app.get('/', (req, res) => {
+    res.send('manufacturer website server running...')
+})
+app.listen(port, () => {
+    console.log('listening to port....');
+})
 
