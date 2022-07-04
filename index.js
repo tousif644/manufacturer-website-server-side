ab909a736722bee6308772e8ea9a65eed2afbb96
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-05 02:16:20 +0600

    Connected to mongo db ignored .env file fetch all tools data

--- index.js
+++ index.js
@@ -10,6 +10,32 @@ app.use(express.json())
 const port = process.env.PORT || 5000;
 
 // apis
+
+// Mongo->DB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szq7h.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// async function
+async function run() {
+    await client.connect();
+
+    // database -> collection
+    const toolsCollection = client.db("ManufacturerWebsite").collection('tools');
+
+
+    // getting all tools api
+    app.get('/tools', async (req, res) => {
+        const query = {};
+        const result = await toolsCollection.find(query).toArray();
+        res.send(result);
+    })
+
+}
+
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
     res.send('manufacturer website server running...')
 })
