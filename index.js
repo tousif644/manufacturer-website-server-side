d72ab7c2380a6ae73196d6e6ea48e808fe03576f
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-07 03:44:18 +0600

    Verify jwt a separate Function declared

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // required modules
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 app.use(cors());
@@ -16,72 +17,100 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.szq7h.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// async function
-async function run() {
-    await client.connect();
+// Verifying jwt token 
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send("Unauthorized Access")
+    }
+    const splitedToken = authHeader.split(" ")[1];
+    console.log("Token is :", splitedToken);
 
-    // database -> collection
-    const toolsCollection = client.db("ManufacturerWebsite").collection('tools');
-    const bookingCollection = client.db("ManufacturerWebsite").collection('cart');
-    const userCollection = client.db("ManufacturerWebsite").collection("users");
-
-    // getting all tools api
-    app.get('/tools', async (req, res) => {
-        const query = {};
-        const result = await toolsCollection.find(query).toArray();
-        res.send(result);
-    })
-
-
-    // getting a tools by using this api
-    app.get('/tools/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await toolsCollection.findOne(query);
-        res.send(result);
-    })
-
-    // getting cart item for specific email
-    app.get('/cart/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = { userEmail: email };
-        const result = await bookingCollection.find(query).toArray();
-        res.send(result)
-    })
-
-    // posting into cart
-    app.post('/cart', async (req, res) => {
-        const booking = req.body;
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result);
-    })
-
-
-    // cart item delete
-    app.delete('/cart/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = { userEmail: email };
-        const result = await bookingCollection.deleteOne(query);
-        res.send(result);
-    })
-
-    // getting all user whoever create an account and giving them jwt token
-    app.put("/users/:email", async (req, res) => {
-        const email = req.params.email;
-        const user = req.body;
-        const filter = { userEmail: email };
-        const options = { upsert: true };
-
-        const updateDoc = {
-            $set: user
+    jwt.verify(splitedToken, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" })
         }
-
-        const result = await userCollection.updateOne(filter, updateDoc, options)
-        res.send(result)
+        req.decoded = decoded
+        next();
     })
 }
 
-run().catch(console.dir)
+    // async function
+    async function run() {
+        await client.connect();
+
+        // database -> collection
+        const toolsCollection = client.db("ManufacturerWebsite").collection('tools');
+        const bookingCollection = client.db("ManufacturerWebsite").collection('cart');
+        const userCollection = client.db("ManufacturerWebsite").collection("users");
+
+        // getting all tools api
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const result = await toolsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
+        // getting a tools by using this api
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolsCollection.findOne(query);
+            res.send(result);
+        })
+
+        // getting cart item for specific email
+        app.get('/cart/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email;
+            const decodedEmail = req.decoded.userEmail;
+            console.log(decodedEmail);
+            if (email === decodedEmail) {
+                const query = { userEmail: email };
+                const result = await bookingCollection.find(query).toArray();
+                res.send(result)
+            } else {
+                return res.status(403).send({ message: "Forbidden Access" })
+            }
+
+        })
+
+        // posting into cart
+        app.post('/cart', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+
+        // cart item delete
+        app.delete('/cart/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // getting all user whoever create an account and giving them jwt token
+        app.put("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { userEmail: email };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: user
+            }
+
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+
+            //jwt signing
+            const token = jwt.sign({ userEmail: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 * 60 * 60 })
+            res.send({ result, token })
+        })
+    }
+
+    run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
@@ -90,4 +119,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('listening to port....');
 })
-
